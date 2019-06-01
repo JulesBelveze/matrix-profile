7e2e4e733ab0afa5d8f7aeca3c1b8a3efc7bebc9
Javid Lakha <javid.lakha@sphericaldefence.com>
2019-06-01 14:43:47 +0100

    setup script

---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import print_function
+
+from glob import glob
+from os.path import basename
+from os.path import splitext
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='matrix_profile',
+    version='1.5.0',
+    author='Javid Lakha',
+    author_email='javid.lakha@outlook.com',
+    url='https://github.com/javidlakha/matrix_profile',
+    packages=find_packages('matrix_profile'),
+    package_dir={'': 'matrix_profile'},
+    py_modules=[splitext(basename(path))[0] for path in glob('matrix_profile/*.py')],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: Unix',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Utilities',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'numpy'
+    ],
+    extras_require={
+    },
+)
